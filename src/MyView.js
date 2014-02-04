7f318f5d53f72dbb0f3a2f3ca28a15ea337cdb0f
Yaroslav Karandashev <yarix7@gmail.com>
2014-02-04 18:10:15 +0300

    Update MyView.js

--- src/MyView.js
+++ src/MyView.js
@@ -1,5 +1,5 @@
 function MyView(selector) {
-	console.log("MyView" + selector);
-	SimpleView.apply(this, arguments);
+  console.log("MyView" + selector);
+  SimpleView.apply(this, arguments);
 }
 MyView.prototype = Object.create(SimpleView.prototype);
