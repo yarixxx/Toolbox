e75348473d6c7061461d71131405ca8bef833ec2
Yaroslav Karandashev <yarix7@gmail.com>
2014-02-05 15:39:30 +0300

    Update MyView.js

--- src/MyView.js
+++ src/MyView.js
@@ -2,4 +2,15 @@ function MyView(selector) {
   console.log("MyView" + selector);
   SimpleView.apply(this, arguments);
 }
+
 MyView.prototype = Object.create(SimpleView.prototype);
+
+MyView.prototype.publishFields = function(data) {
+	var table = document.querySelector(this.selector + " table");
+	var fields = "";
+	var itemTemplate = new Template("<tr><td>{key}</td><td>{value}</td></tr>");
+	for (var key in data) {
+		fields += itemTemplate.render({ key: key, value: data[key] });
+	}
+	table.innerHTML = fields;
+}
