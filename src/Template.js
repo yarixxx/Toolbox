34c9ad01d02530fd6f6882943297ca19581b1847
Yaroslav Karandashev <yarix7@gmail.com>
2014-02-14 12:45:24 +0300

    Update Template.js

--- src/Template.js
+++ src/Template.js
@@ -1,12 +1,25 @@
-function Template(template) {
-  return {
-    _tpl: template,
-    render: function(data) {
-      var result = this._tpl;
-      for (var key in data) {
-        result = result.replace(new RegExp("\{"+key+"\}", "g"), data[key]);
-      }
-      return result;
-    }
+/**
+ * It creates an instance of Template object
+ * 
+ * @constructor
+ * @param template {String}
+ * @returns {Cart.Template}
+ */
+
+Cart.Template = function(template) {
+  this._tpl = template;
+};
+
+/**
+ * Produces a text based on the template and provided key-value map
+ * 
+ * @param data {Object}
+ * @returns {String}
+ */
+Cart.Template.prototype.render = function(data) {
+  var result = this._tpl;
+  for (var key in data) {
+    result = result.replace(new RegExp("\{"+key+"\}", "g"), data[key]);
   }
-}
+  return result;
+};
