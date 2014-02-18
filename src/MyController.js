6dd73ca735c43b392edbad3d1ae154f62d87293f
Yaroslav Karandashev <yarix7@gmail.com>
2014-02-18 17:10:24 +0300

    Update MyController.js

--- src/MyController.js
+++ src/MyController.js
@@ -1,17 +1,20 @@
-function MyController() {
-  this.view = null;
-  this.template = new Template("http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units={units}");
-  this.urlRetriever = new UrlRetriever(this.template);
-  this.timer = false;
+function MyController(model) {
+  this.model = model;
+  this.dateProvider = null;
 }
 
-MyController.prototype.run = function() {
-  this.urlRetriever.run(this, { city: "Petersburg", country: "ru" , units: "metric"}, function(data) {
-	this.view.publishFields(data.main);
-  });
-	
-  if (!this.timer) {
-	setInterval(this.run.bind(this), 60000);
-	this.timer = true;
-  }
+MyController.prototype.setDateProvider = function(dateProvider) {
+  this.dateProvider = dateProvider;
+}
+
+MyController.prototype.dayUpdate = function() {
+  this.model.newDay(this.dateProvider.getDay());
+}
+
+MyController.prototype.run = function(interval) {
+  this.timer = setInterval(this.dayUpdate, interval);
+}
+
+MyController.prototype.stop = function(interval) {
+  clearInterval(this.timer);
 }
