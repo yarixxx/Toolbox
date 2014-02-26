afbca47187cddfa9e7c4f3dec2a8b65523151208
Yaroslav Karandashev <yarix7@gmail.com>
2014-02-26 14:28:58 +0300

    Update UseDOM.js

--- src/UseDOM.js
+++ src/UseDOM.js
@@ -2,9 +2,20 @@ function UseDOM() {
 }
 
 UseDOM.prototype.readValue = function(selector) {
-  return document.querySelector(selector).innerText;
+  return document.querySelector(selector).innerText
 }
 
 UseDOM.prototype.putValue = function(selector, value) {
   document.querySelector(selector).innerText = value;
+}
+
+UseDOM.prototype.script = function(name, callback) {
+  var script = document.createElement("script");
+  document.querySelector("head").appendChild(script);
+  script.onload = script.onreadystatechange = function() {
+    script.onload = script.onreadystatechange = null;
+    callback();
+  };
+  script.setAttribute("async", true);
+  script.setAttribute("src", "src/" + name + ".js");
 }
