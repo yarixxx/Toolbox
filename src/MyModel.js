26b05f0256b175ec61a81afc385d0ac25bbe2083
Yaroslav Karandashev <yarix7@gmail.com>
2014-02-26 14:24:58 +0300

    Update MyModel.js

--- src/MyModel.js
+++ src/MyModel.js
@@ -1,10 +1,10 @@
 /**
  *
- * @param view
  * @constructor
  */
-function MyModel(view) {
-  this.view = view;
+function MyModel() {
+  this.view = null;
+  this.controller = null;
   this.day = null;
 };
 
@@ -12,3 +12,15 @@ MyModel.prototype.newDay = function(day){
   this.day = day;
   this.view.update({ day: day });
 };
+
+MyModel.prototype.setMyController = function(controller) {
+  this.controller = controller;
+}
+
+MyModel.prototype.setMyView = function(view) {
+  this.view = view;
+}
+
+MyModel.prototype.getDependencies = function() {
+    return ["MyController", "MyView"];
+}
